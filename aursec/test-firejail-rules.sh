03200e8c7c369cf8e68743419b5b0e41a4d038eb
Bennett Piater <bennett@piater.name>
2016-12-06 09:43:30 +0100

    test-firejail-rules.sh: add test case and clean up the code

--- aursec/test-firejail-rules.sh
+++ aursec/test-firejail-rules.sh
@@ -8,30 +8,38 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 # Copyright (c) 2016-2017 Lukas Krismer and Bennett Piater.
 declare -a test_case_names failed_tests
-declare -A pkgbuilds test_conditions cleanups
+declare -A pkgbuilds test_conditions cleanup_funcs
 
 source /usr/share/makepkg/util.sh
 colorize
 # Output the firejail ruleset
 plain "Testing the following firejail options:"
 grep "readonly firejail_opts=" "$(which aursec-hash)"
+grep "firejail " "$(which aursec-hash)"
 
 ################
 # Define tests #
 ################
 # Create a file in /tmp
-name="write to /tmp"
+name="disallow writes to /tmp"
 test_case_names+=("$name")
 test_conditions["$name"]="! stat /tmp/evil_file"
-cleanups["$name"]="rm -f /tmp/evil_file"
+cleanup_funcs["$name"]="rm -f /tmp/evil_file"
 pkgbuilds["$name"]="touch /tmp/evil_file"
 
 # Create a file in $HOME
-name="write to /home"
+name="disallow writes to /home"
 test_case_names+=("$name")
 test_conditions["$name"]="! stat ~/evil_file"
-cleanups["$name"]="rm -f ~/evil_file"
+cleanup_funcs["$name"]="rm -f ~/evil_file"
 pkgbuilds["$name"]="touch ~/evil_file"
+
+# Create a file in the directory of the pkgbuild
+name="allow writes to the source folder"
+test_case_names+=("$name")
+test_conditions["$name"]="stat good_file"
+cleanup_funcs["$name"]="rm -f good_file"
+pkgbuilds["$name"]="touch good_file"
 
 #################
 # Execute tests #
@@ -54,7 +62,7 @@ for test_case in "${test_case_names[@]}"; do
 	# Check whether this test failed
 	eval "${test_conditions[$test_case]}" >/dev/null 2>&1 || failed_tests+=("$test_case")
 	# Clean up
-	eval "${cleanups[$test_case]}" >/dev/null
+	eval "${cleanup_funcs[$test_case]}" >/dev/null
 	rm -rf ./* >/dev/null 2>&1
 	rm -rf .* >/dev/null 2>&1
 done
