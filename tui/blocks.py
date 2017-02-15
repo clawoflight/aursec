82458af71633f1322cd85b3a74b21cdf18c3b66b
Lukas Krismer <l.krismer@student.uibk.ac.at>
2017-02-15 15:30:02 +0100

    blocks: added backend for tui

--- tui/blocks.py
+++ tui/blocks.py
@@ -46,7 +46,24 @@ class Blocks
 	    transactions = ""
 	    for i in xrange(0,number_transactions):
 	    	transaction += json_result_to_string(["result"]["transactions"][i])
-	    self.all_blocks.append(Block(number,miner,sender,transactions))
+	    	if i != number_transactions-1:
+	    		transactions += " | "
+	    block = Block(number,miner,sender,transactions)
+	    self.all_blocks.append(block)
+	    if(miner == self.user)
+	    	self.mine_blocks.append(block)
+	    if(number_transactions != 0)
+	    	self.transactions.append(block)
+
+	def json_result_to_string(self,transaction)
+		code = str(transaction["input"])[10:]
+		first = int(code[1:64],16)
+		second = int(code [65:128],16)
+		len_id = int(code[first*2:first*2+64])
+		id=bytearray.fromhex(code[(first*2+64):(first*2+64+len_id*2)]).decode().rstrip()
+		len_hash = int(code[second*2:second*2+64])
+		hash=bytearray.fromhex(code[(second*2+64):(second*2+64+len_hash*2)]).decode().rstrip()
+		return ("ID: " + id +" hash: " + hash)
 
 
 
